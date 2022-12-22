a82f80c749467a16aec3f3b6eec1b446ab2040d7
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-12-22 13:17:00 -0500

    ⚡ [UPDATE] CRUD

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo, ObjectId
 from flask_cors import CORS
 from config import config
@@ -6,40 +6,62 @@ from config import config
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = config["MONGO_URI"]
+
+CORS(app)
 mongo = PyMongo(app)
+
 db = mongo.db.users
 
 
 @app.route("/users", methods=["GET"])
 def get_users():
-    return "received"
+    users = []
+    for user in db.find():
+        users.append({
+            "_id": str(user["_id"]),
+            "name": user["name"],
+            "email": user["email"],
+            "password": user["password"],
+        })
+    return jsonify(users)
 
 
-@app.route("/users/<id>", methods=["GET"])
-def get_user():
-    return "received"
+@app.route("/user/<id>", methods=["GET"])
+def get_user(id):
+    user = db.find_one({"_id": ObjectId(id)})
+    if user is not None:
+        user["_id"] = str(user["_id"])
+    else:
+        user = {}
+    return jsonify(user=user)
 
 
 @app.route("/users", methods=["POST"])
 def create_user():
-    # id = db.insert({
-    #     "name": request.json["name"],
-    #     "email": request.json["email"],
-    #     "password": request.json["password"],
-    # })
-    # print("id: ", str(ObjectId(id)))
-    print("db: ", db)
-    return "received"
+    user = db.insert_one({
+        "name": request.json["name"],
+        "email": request.json["email"],
+        "password": request.json["password"],
+    })
+    return jsonify(id=str(user.inserted_id), message="user created sucessfully.")
 
 
-@app.route("/users", methods=["DELETE"])
-def delete_user():
-    return "received"
+@app.route("/user/<id>", methods=["DELETE"])
+def delete_user(id: str):
+    db.delete_one({'_id': ObjectId(id)})
+    return jsonify(message="user deleted", id=id)
 
 
-@app.route("/users/<id>", methods=["PUT"])
-def update_user():
-    return "received"
+@app.route("/user/<id>", methods=["PUT"])
+def update_user(id: str):
+    db.update_one({'_id': ObjectId(id)}, {
+        "$set" : {
+            'name': request.json["name"],
+            'email': request.json["email"],
+            'password': request.json["password"]
+        }
+    })
+    return jsonify(message="user updated", id=id)
 
 
 if __name__ == "__main__":
--- backend/src/config/__init__.py
+++ backend/src/config/__init__.py
@@ -7,4 +7,6 @@ config = os.environ
 DB_NAME = config.get("DB_NAME", "")
 DB_PORT = config.get("DB_PORT", 27017)
 DB_HOST = config.get("DB_HOST", "localhost")
-config["MONGO_URI"] = f"mongodb://${DB_HOST}:{DB_PORT}/{DB_NAME}"
+DB_USER = config.get("MONGO_USERNAME", "")
+DB_PASSWORD = config.get("MONGO_PASSWORD", "")
+config["MONGO_URI"] = f"mongodb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?authSource=admin"
