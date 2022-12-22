5ee050d9042f5add71a77b6bd30ef3952ad9721f
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-12-22 13:22:14 -0500

    🎨 [ADD] Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
